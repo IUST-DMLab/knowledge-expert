141992aac4a57e8766edb02d59496d268cc4b766
HosseiN Khademi Khaledi <hossein.khaledi@yahoo.com>
2017-04-09 16:05:46 +0430

    Add first page

--- web/src/main/java/ir/ac/iust/dml/kg/knowledge/expert/web/MainApplication.java
+++ web/src/main/java/ir/ac/iust/dml/kg/knowledge/expert/web/MainApplication.java
@@ -1,7 +1,18 @@
 package ir.ac.iust.dml.kg.knowledge.expert.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
 /**
  * MainApplication of web
  */
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
