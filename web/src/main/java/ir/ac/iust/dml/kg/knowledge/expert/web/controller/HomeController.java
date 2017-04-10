90fdf13509243073b04bdb0c7be661235b214511
HosseiN Khademi Khaledi <hossein.khaledi@yahoo.com>
2017-04-10 15:23:48 +0430

    Enable Security

--- web/src/main/java/ir/ac/iust/dml/kg/knowledge/expert/web/controller/HomeController.java
+++ web/src/main/java/ir/ac/iust/dml/kg/knowledge/expert/web/controller/HomeController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String viewHome() {
-        return "index2";
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
