4bb3fdbc8372f0a0bd306b8b83be9c227b3a92ec
HosseiN Khademi Khaledi <hossein.khaledi@yahoo.com>
2017-06-05 06:56:06 +0430

    Check user vote on his ticket

---
+++ web/src/main/java/ir/ac/iust/dml/kg/knowledge/expert/web/services/v1/ReportsServiceImpl.java
@@ -0,0 +1,55 @@
+package ir.ac.iust.dml.kg.knowledge.expert.web.services.v1;
+
+import ir.ac.iust.dml.kg.knowledge.commons.PagingList;
+import ir.ac.iust.dml.kg.knowledge.expert.access.dao.IReportDao;
+import ir.ac.iust.dml.kg.knowledge.expert.access.dao.IUserDao;
+import ir.ac.iust.dml.kg.knowledge.expert.access.entities.Ticket;
+import ir.ac.iust.dml.kg.knowledge.expert.access.entities.User;
+import ir.ac.iust.dml.kg.knowledge.expert.access.stats.KeyCount;
+import ir.ac.iust.dml.kg.knowledge.expert.access.stats.UserStats;
+import ir.ac.iust.dml.kg.knowledge.store.client.Vote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.jws.WebService;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ */
+@WebService(endpointInterface = "ir.ac.iust.dml.kg.knowledge.expert.web.services.v1.IReportService")
+public class ReportsServiceImpl implements IReportService {
+    @Autowired
+    private IUserDao userDao;
+    @Autowired
+    private IReportDao reportDao;
+
+    @Override
+    public List<String> login() {
+        final Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        final List<String> result = new ArrayList<>();
+        authorities.forEach(a -> result.add(a.getAuthority()));
+        return result;
+    }
+
+    @Override
+    public PagingList<Ticket> triples(String username, String subject, Boolean hasVote, Vote vote, int page, int pageSize) {
+        User user = username != null ? userDao.readByUsername(username) : null;
+        return reportDao.searchTicketState(user, subject, hasVote, vote, page, pageSize);
+    }
+
+    @Override
+    public PagingList<KeyCount> subjects(String username, Boolean hasVote, Vote vote, int page, int pageSize) {
+        User user = username != null ? userDao.readByUsername(username) : null;
+        return reportDao.searchSubjectState(user, hasVote, vote, page, pageSize);
+    }
+
+    @Override
+    public PagingList<UserStats> users(String username, Boolean hasVote, Vote vote, int page, int pageSize) {
+        User user = username != null ? userDao.readByUsername(username) : null;
+        return reportDao.searchUserState(user, hasVote, vote, page, pageSize);
+    }
+}
