da661aef0364c3d6fbbdc0f5a18a6c8d771bc05e
Majid Asgari <majid.asgari@gmail.com>
2017-11-04 17:01:54 +0330

    add date to supported value type of triples.

--- access/src/main/java/ir/ac/iust/dml/kg/knowledge/store/client/ValueType.java
+++ access/src/main/java/ir/ac/iust/dml/kg/knowledge/store/client/ValueType.java
@@ -8,5 +8,5 @@ package ir.ac.iust.dml.kg.knowledge.store.client;
  * Types of value
  */
 public enum ValueType {
-    Resource, String, Boolean, Byte, Short, Integer, Long, Double, Float;
+    Resource, String, Boolean, Byte, Short, Integer, Long, Double, Float, Date;
 }
